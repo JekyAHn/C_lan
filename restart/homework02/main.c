12d669279529e2dc1d7a67760bee6cbba64eabd0
JekyAHn <lemon1576@naver.com>
2023-05-19 18:11:24 +0900

    202327057 안제철

--- restart/homework02/main.c
+++ restart/homework02/main.c
@@ -21,37 +21,37 @@ int main()
         scanf("%s",&input);
         if (input == '2')
         {
-        clearbuffer(screen, width, height);
-        guideBuffer(screen, width, height);
-
-        system("cls");
-        printf("%s\nPlease input>", screen);
-
-        scanf("%s", &input);
-        if (input == '1')
-        {
             clearbuffer(screen, width, height);
-            TitleBuffer(screen, width, height);
-        }
+            guideBuffer(screen, width, height);
+
+            system("cls");
+            printf("%s\nPlease input>", screen);
+
+            scanf("%s", &input);
+            if (input == '1')
+             {
+                clearbuffer(screen, width, height);
+                TitleBuffer(screen, width, height);
+             }
         }
         else if (input == '3')
         {
-        clearbuffer(screen, width, height);
-        GameOverBuffer(screen, width, height);
-
-        system("cls");
-        printf("%s\nPlease input>", screen);
-
-        scanf("%s", &input);
-        if (input == '1')
-        {
-            game = 0;
-        }
-        else if (input == '2')
-        {
             clearbuffer(screen, width, height);
-            TitleBuffer(screen, width, height);
-        }
+            GameOverBuffer(screen, width, height);
+
+            system("cls");
+            printf("%s\nPlease input>", screen);
+
+            scanf("%s", &input);
+            if (input == '1')
+            {
+                game = 0;
+            }
+            else if (input == '2')
+            {
+                clearbuffer(screen, width, height);
+                TitleBuffer(screen, width, height);
+            }
         
         }
         
